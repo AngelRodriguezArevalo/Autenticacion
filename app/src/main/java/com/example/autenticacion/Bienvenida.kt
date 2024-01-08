132b87a04dbd35abc71812adc4771d73c9939106
angelrodriguezdam@hotmail.com <angelrodriguezdam@hotmail.com>
2024-01-08 10:18:34 +0100

    base de datos con firebase

--- app/src/main/java/com/example/autenticacion/Bienvenida.kt
+++ app/src/main/java/com/example/autenticacion/Bienvenida.kt
@@ -3,8 +3,10 @@ package com.example.autenticacion
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.autenticacion.databinding.ActivityBienvenidaBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class Bienvenida : AppCompatActivity() {
     lateinit var binding: ActivityBienvenidaBinding
@@ -13,7 +15,7 @@ class Bienvenida : AppCompatActivity() {
         val binding = ActivityBienvenidaBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        
+        val db = FirebaseFirestore.getInstance()
 
         binding.botonCerrarsesion.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
@@ -21,7 +23,30 @@ class Bienvenida : AppCompatActivity() {
             startActivity(Intent(this, MainActivity::class.java))
         }
 
+        binding.botonGuardarCoche.setOnClickListener {
+            if (binding.marcaCoche.text.isNotEmpty() &&
+                binding.matriculaCoche.text.isNotEmpty() &&
+                binding.modeloCoche.text.isNotEmpty()&&
+                binding.colorCoche.text.isNotEmpty()){
 
+                db.collection("coches").add(mapOf(
+                    "color" to binding.colorCoche.text,
+                    "marca" to binding.marcaCoche.text,
+                    "matricula" to binding.matriculaCoche.text,
+                    "modelo" to binding.modeloCoche.text
+                ))
+
+                    .addOnSuccessListener { documento ->
+                        Toast.makeText(this, "Nuevo coche añadido con el id: ${documento.id}", Toast.LENGTH_SHORT).show()
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(this, "Error en la inserción del coche", Toast.LENGTH_SHORT).show()
+                    }
+
+            }else{
+                Toast.makeText(this , "Algún campo está vacío", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
 
